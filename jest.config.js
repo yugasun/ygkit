a600ab94a46d0513ae5061f508d5cb52fcd8e747
yugasun <yuga_sun@163.com>
2020-11-20 11:27:18 +0800

    fix(object): add typeOf & deepClone

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,7 @@ module.exports = {
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  // testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  testRegex: '(/__tests__/deep-clone.(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
--- packages/object/src/index.ts
+++ packages/object/src/index.ts
@@ -1,1 +1,3 @@
 export * from './utils/get-prop';
+export * from './utils/type-of';
+export * from './utils/deep-clone';
